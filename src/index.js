8a473903f0fb47674acb0dd1ecd4ab07b21b375b
Alemba-Vivian <alembavivian@gmail.com>
2023-11-02 23:25:38 +0300

    made changes to the UI

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 import './STYLES/style.css';
 
+//DOM MANIPULATION--REFERENCES
 // const input = document.querySelector('.myInput');
 const input = prompt("Enter the name of the location", "Kenya");
 const img = document.querySelector('img');
+const img2 = document.querySelector('#img2');
+
 const container = document.querySelector('.container');
 
+//LOCATION
 const country = document.querySelector('.location-country');
+const country2 = document.querySelector('.location-country2');
+const country3 = document.querySelector('.location-country3');
+
 const localTime = document.querySelector('.localtime');
+const localTime2 = document.querySelector('.localtime2');
+
 const localtimeEpoch = document.querySelector('.localtime_epoch');
 const weather_text = document.querySelector('.text');
+
 const name = document.querySelector('.name');
+const name2 = document.querySelector('.name2');
+
 const region = document.querySelector('.region');
 const tzID = document.querySelector('.tz_id');
 const lat = document.querySelector('.lat');
 const lon = document.querySelector('.lon');
 
-// name.textContent ="";
-// region.textContent ="";
-// tzID.textContent ="";
+//CURRENT
+const pressure_in = document.querySelector('.pressure_in');
+const pressure_mb = document.querySelector('.pressure_mb');
+const uv = document.querySelector('.uv');
+const precip_in = document.querySelector('.precip_in');
+const precip_mm = document.querySelector('.precip_mm');
+const temp_c = document.querySelector('.temp_c');
+const temp_f = document.querySelector('.temp_f');
+
+
 
 
 async function fetchWeatherAPI(){
@@ -26,16 +45,41 @@ async function fetchWeatherAPI(){
     console.log(weatherData); 
 
     img.src = weatherData.current.condition.icon;
-    weather_text.textContent =weatherData.current.condition.text;
-
+    img2.src = weatherData.current.condition.icon;
+  
+    //location
     country.textContent =`${weatherData.location.country}`;
+    country2.textContent =`${weatherData.location.country}`;
+    country3.textContent =`${weatherData.location.country}`;
+
     localTime.textContent =`${weatherData.location.localtime}`;
+    localTime2.textContent =`${weatherData.location.localtime}`;
+
+
+
     localtimeEpoch.textContent =`${weatherData.location.localtime_epoch}`;
     lat.textContent =`${weatherData.location.lat}`;
     lon.textContent =`${weatherData.location.lon}`;
+
     name.textContent =`${weatherData.location.name}`;
+    name2.textContent =`${weatherData.location.name}`;
+
     region.textContent =`${weatherData.location.region}`;
     tzID.textContent =`${weatherData.location.tz_id}`;
+
+    //current
+    weather_text.textContent =weatherData.current.condition.text;
+    pressure_in.textContent =`${weatherData.current.pressure_in}`;
+    pressure_mb.textContent =`${weatherData.current.pressure_mb}`;
+    uv.textContent =`${weatherData.current.uv}`;
+    precip_in.textContent =`${weatherData.current.precip_in}`;
+    precip_mm.textContent =`${weatherData.current.precip_mm}`;
+    temp_c.textContent =`${weatherData.current.temp_c}`;
+    temp_f.textContent =`${weatherData.current.temp_f}`;
+
+
+
+
     
     // if(weatherData.current.condition.text==='Sunny'){
     //     container.style.backgroundColor ='red';
